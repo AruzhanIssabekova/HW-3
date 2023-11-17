502a574006b5ee33dfc3e11846c11d92fa86fbf4
AruzhanIssabekova <aruzhan.issabekova123@gmail.com>
2023-11-17 21:27:50 +0600

    add HW3

---
+++ HW3/HW3.py
@@ -0,0 +1,26 @@
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a+b>c or a+c>b or b+c>a:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# if a%2==0:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# if a+b>c:
+#     print(True)
+# else:
+#     print(False)
+
+# a=int(input())
+# b=int(input())
+# if a>b:
+#     print(True)
+# else:
+#     print(False)
